ef6fab1f97ca16f201b6bb85396f0e49d6f83469
Shane <j8yeschph@mozmail.com>
2025-06-09 11:53:21 -0700

    Increased storage size

--- src/helpers/checksum.ts
+++ src/helpers/checksum.ts
@@ -5,7 +5,7 @@ import { blake3 } from '@noble/hashes/blake3';
 export function getChecksum(binaryString: string): string {
   const buffer = Buffer.from(binaryString, 'binary');
   const hash = blake3(buffer);
-  return (hash[0]! & 0x1f).toString(2).padStart(5, '0'); // Return the first byte masked to 5 bits
+  return (hash[0]! & 0xff).toString(2).padStart(8, '0'); // Return the first byte masked to 8 bits
 }
 
 // export function getChecksumFromBinary(binaryString: string): number {
--- src/helpers/padding.ts
+++ src/helpers/padding.ts
@@ -5,11 +5,11 @@ export function padBinaryString(binaryString: string): string {
   assert(binaryString.length % 8 === 0, 'Binary string length must be a multiple of 8');
 
   const checksum: string = getChecksum(binaryString);
-  assert(checksum.length === 5, 'Checksum must be 5 bits long');
+  assert(checksum.length === 8, 'Checksum must be 8 bits long');
 
   const originalLength: number = binaryString.length / 8;
-  const encodedLength: string = originalLength.toString(2).padStart(6, '0');
-  assert(encodedLength.length === 6, 'Encoded length must be 6 bits long');
+  const encodedLength: string = originalLength.toString(2).padStart(14, '0');
+  assert(encodedLength.length === 14, 'Encoded length must be 14 bits long');
   binaryString += encodedLength;
   binaryString += checksum;
 
@@ -21,16 +21,16 @@ export function padBinaryString(binaryString: string): string {
 export function unpadBinaryString(paddedBinaryString: string): string {
   assert(paddedBinaryString.length % 11 === 0, 'Padded binary string length must be a multiple of 11');
 
-  const encodedLength: string = paddedBinaryString.slice(-11, -5);
-  assert(encodedLength.length === 6, 'Encoded length must be 6 bits long');
+  const encodedLength: string = paddedBinaryString.slice(-22, -8);
+  assert(encodedLength.length === 14, 'Encoded length must be 14 bits long');
 
   const originalLength: number = parseInt(encodedLength, 2);
-  const start: number = paddedBinaryString.length - 11 - originalLength * 8;
-  const unpaddedBinaryString: string = paddedBinaryString.slice(start, -11);
+  const start: number = paddedBinaryString.length - 22 - originalLength * 8;
+  const unpaddedBinaryString: string = paddedBinaryString.slice(start, -22);
   assert(unpaddedBinaryString.length % 8 === 0, 'Unpadded binary string length must be a multiple of 8');
 
   const checksum: string = getChecksum(unpaddedBinaryString);
-  assert(checksum === paddedBinaryString.slice(-5), 'Checksum does not match');
+  assert(checksum === paddedBinaryString.slice(-8), 'Checksum does not match');
 
   return unpaddedBinaryString;
 }
