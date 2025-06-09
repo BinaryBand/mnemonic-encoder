1b37bcf7b2b80f251a849fe9dc4b67d524b30c23
Shane <j8yeschph@mozmail.com>
2025-06-09 11:19:09 -0700

    Added checksum

--- src/helpers/padding.ts
+++ src/helpers/padding.ts
@@ -4,14 +4,14 @@ import { getChecksum } from '@/helpers/checksum';
 export function padBinaryString(binaryString: string): string {
   assert(binaryString.length % 8 === 0, 'Binary string length must be a multiple of 8');
 
+  const checksum: string = getChecksum(binaryString);
+  assert(checksum.length === 5, 'Checksum must be 5 bits long');
+
   const originalLength: number = binaryString.length / 8;
   const encodedLength: string = originalLength.toString(2).padStart(6, '0');
   assert(encodedLength.length === 6, 'Encoded length must be 6 bits long');
   binaryString += encodedLength;
-  binaryString += '0'.repeat(5); // Append 5 bits for checksum
-
-  const checksum = getChecksum(binaryString);
-  assert(checksum.length === 5, 'Checksum must be 5 bits long');
+  binaryString += checksum;
 
   const paddingLength: number = (11 - (binaryString.length % 11)) % 11;
   const padding: string = '0'.repeat(paddingLength);
@@ -26,5 +26,11 @@ export function unpadBinaryString(paddedBinaryString: string): string {
 
   const originalLength: number = parseInt(encodedLength, 2);
   const start: number = paddedBinaryString.length - 11 - originalLength * 8;
-  return paddedBinaryString.slice(start, -11);
+  const unpaddedBinaryString: string = paddedBinaryString.slice(start, -11);
+  assert(unpaddedBinaryString.length % 8 === 0, 'Unpadded binary string length must be a multiple of 8');
+
+  const checksum: string = getChecksum(unpaddedBinaryString);
+  assert(checksum === paddedBinaryString.slice(-5), 'Checksum does not match');
+
+  return unpaddedBinaryString;
 }
